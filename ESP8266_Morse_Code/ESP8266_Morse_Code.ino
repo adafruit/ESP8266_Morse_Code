4043f058e0c18a0c69ad1ef09b74b7e0b60357ad
Tony DiCola <tony@tonydicola.com>
2015-04-08 19:26:48 -0700

    Switch to bootstrap in HTML, and add URL form decoding.

--- ESP8266_Morse_Code/ESP8266_Morse_Code.ino
+++ ESP8266_Morse_Code/ESP8266_Morse_Code.ino
@@ -1,22 +1,24 @@
 // ESP8266 Morse Code Webserver Example
+//
 // Display a web page with a form and blink out the submitted
 // message in morse code on an LED!
-// Author: Tony DiCola
-// License: MIT License
 //
 // Note you need to have a LED connected to pin 13 to see the
 // morse code messages.
-
+//
+// Author: Tony DiCola
+//
+// License: MIT License
+//
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
 #include "index_html.h"
 
 
 // Details of AP to connect to on boot. 
-const char* ssid = "your WiFi AP name here";
-const char* password = "your WiFi AP password here";
+const char* ssid = "your AP name here...";
+const char* password = "your AP password here...";
 
 // Pin connected to the LED.
 const int led = 13;
@@ -40,6 +42,9 @@ const int morse_word_delay_ms = morse_dot_ms*7;
 // but you can reverse things if your circuit is different.
 const int led_on  = HIGH;
 const int led_off = LOW;
+
+// Maximum length of message, used for internal buffer size.
+const int max_message_len = 500;
 
 // Table of character to morse code mapping.
 // Each string represents the dashes and dots for a character.
@@ -106,18 +111,35 @@ void setup(void)
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-  // Configure web server root handler to return the main HTML page.
-  // Note the funny []() { .. code .. } syntax is a C++11 lambda function.
+  // Configure web server root handler to return the main 
+  // HTML page.  Note the funny []() { .. code .. } syntax
+  // is a C++11 lambda function, you can learn more about
+  // the syntax here:
+  //   http://www.drdobbs.com/cpp/lambdas-in-c11/240168241   
   server.on("/", []() {
-    // Server up the main HTML page.
+    // Serve up the main HTML page.
     server.send(200, "text/html", index_html);
   });
   
   // Configure a handler for the /morse endpoint.
   server.on("/morse", []() {
-    // Blink out the message in morse code, then respond with the root 
-    // HTML page again.
-    blink_morse(led, server.arg("message").c_str());
+    // Blink out the message in morse code, then respond 
+    // with the root HTML page again.
+
+    // Grab the message form parameter and fail if it's
+    // larger than our max allowed size.
+    const char* message = server.arg("message").c_str();
+    if (strlen(message) >= max_message_len) {
+      server.send(500, "text/plain", "Message is too long!");
+      return;
+    }
+    // Perform form URL decoding to get the plain message.
+    char decoded[max_message_len] = {0};
+    form_url_decode(message, decoded);
+    // Print and blink the message!
+    Serial.print("Blinking message: "); Serial.println(decoded);
+    blink_morse(led, decoded);
+    // Return the main page again.
     server.send(200, "text/html", index_html);
   });
   
@@ -156,32 +178,34 @@ void blink_morse_char(int led_pin, char c) {
      // Turn the LED on, delay for the appropriate dot / dash time,
      // then turn the LED off again.
      digitalWrite(led_pin, led_on);
-     delay(code[i] == '.' ? morse_dot_ms : morse_dash_ms);
+     delay((code[i] == '.') ? morse_dot_ms : morse_dash_ms);
      digitalWrite(led_pin, led_off);
      // Delay between characters.
      delay(morse_char_delay_ms);
   }
   // Handle the last character without any delay after it.
   digitalWrite(led_pin, led_on);
-  delay(code[strlen(code)-1] == '.' ? morse_dot_ms : morse_dash_ms);
+  delay((code[strlen(code)-1] == '.') ? morse_dot_ms : morse_dash_ms);
   digitalWrite(led_pin, led_off);
 }
 
 void blink_morse(int led_pin, const char* message) {
   // Blink out the morse code version of the message on the LED.
   
-  // Process each character in the message and send them out as morse code.
-  // Keep track of the previously seen character to find word boundaries.
+  // Process each character in the message and send them out 
+  // as morse code. Keep track of the previously seen character
+  // to find word boundaries.
   char old = toupper(message[0]);
   blink_morse_char(led_pin, old);
   for (int i = 1; i < strlen(message); ++i) {
     char c = toupper(message[i]);
-    // Delay for word boundary if last char is whitespace and new char
-    // is alphanumeric.
+    // Delay for word boundary if last char is whitespace and 
+    // new char is alphanumeric.
     if (isspace(old) && isalnum(c)) {
       delay(morse_word_delay_ms); 
     }
-    // Delay for letter boundary if both last and new char are alphanumeric.
+    // Delay for letter boundary if both last and new char 
+    // are alphanumeric.
     else if (isalnum(old) && isalnum(c)) {
       delay(morse_letter_delay_ms);
     }
@@ -193,3 +217,47 @@ void blink_morse(int led_pin, const char* message) {
   }
 }
 
+void form_url_decode(const char* encoded, char* decoded) {
+  // Decode a string in 'application/x-www-form-urlencoded' format
+  // to its normal ASCII representation.  See details on this
+  // format in the HTML spec at:
+  //   http://www.w3.org/MarkUp/html-spec/html-spec_8.html#SEC8.2.1
+  // Note the output string MUST be large enough to hold the string!
+  // As long as the output is at least as large as the input then
+  // you will never have a problem (i.e. decoded strings are smaller).
+  
+  // Go through each character and trasform it into the decoded
+  // output as appropriate.
+  for (int i = 0, j = 0; i < strlen(encoded); ++i, ++j) {
+    char c = encoded[i];
+    // The + character turns into a space.
+    if (c == '+') {
+      decoded[j] = ' '; 
+    }
+    // The % character indicates a special character in the
+    // form of '%HH' where HH are the hex digits that represent
+    // the character to use.
+    else if (c == '%') {
+      // If there isn't enough input data to read the next two
+      // characters then just stop because this string is malformed.
+      if (i >= strlen(encoded)-2) {
+        return; 
+      }
+      // Grab the next two encoded characters.
+      char high = toupper(encoded[++i]);
+      char low  = toupper(encoded[++i]);
+      // Skip this character if the encoded characters aren't hex.
+      if (!isxdigit(high) || !isxdigit(low)) {
+        continue;
+      }
+      // Convert high and low hex digits to the character
+      // represented by their value.
+      decoded[j] = (low > '9') ? (low-'A')+10 : low-'0';
+      decoded[j] += 16*((high > '9') ? (high-'A')+10 : high-'0');
+    }
+    // All other encoded characters are just copied over.
+    else {
+      decoded[j] = c;
+    }
+  } 
+}
--- ESP8266_Morse_Code/index_html.h
+++ ESP8266_Morse_Code/index_html.h
@@ -1,24 +1,33 @@
 // Just save the contents of the main HTML page as a string in flash memory.
-// Remember this can't insanely big, there's only 512KB total flash!
+// Remember this can't be insanely big, there's only 512KB total flash!
 
 // Note the raw string literal R syntax is C++11 only!
 const char* index_html = R"(
-  <!doctype html>
+  <!DOCTYPE html>
   <html>
     <head>
       <title>Morse Code Blinker</title>
+      <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+      <style>
+        body {
+          padding-top: 50px;
+          padding-bottom: 20px;
+        }
+      </style>
     </head>
     <body>
-      <h1>Morse Code Blinker</h1>
-      <form action="/morse" method="post">
-        <div>
-          <label for="message">Message:</label>
-          <input type="text" id="message" name="message">
+      <div class="container">
+        <div class="jumbotron">
+          <h1>Morse Code Blinker</h1>
+          <p>Submit text to have it blinked out as morse code on the LED!</p>
+          <form action="/morse" method="post">
+            <div class="form-group">
+              <input class="form-control input-lg" type="text" id="message" name="message" placeholder="Enter message...">
+            </div>
+            <button class="btn btn-lg btn-primary" type="submit">Blink it!</button>
+          </form>
         </div>
-        <div class="button">
-          <button type="submit">Blink!</button>
-        </div>
-      </form>
+      </div>
     </body>
   </html>
 )";
